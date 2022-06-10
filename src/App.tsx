67e59bf5eeb7e886720e10d2aa1ceee79762eba3
SosukeHosokawa <so298go@gmail.com>
2022-06-10 17:54:50 +0900

    update routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,3 @@
-import { useEffect, useState, useRef } from "react";
-import { Socket, Channel } from "phoenix";
-
-import environment from "./types/env";
 import { Route, BrowserRouter, Routes } from "react-router-dom";
 import ManualSignaling from "./pages/ManualSignaling";
 import AutoSignaling from "./pages/AutoSignaling";
@@ -11,8 +7,9 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path={"/manual"} element={<ManualSignaling />}/>
-          <Route path={"/auto"} element={<AutoSignaling />}/>
+          <Route path={"/"} element={<AutoSignaling />} />
+          <Route path={"/manual"} element={<ManualSignaling />} />
+          <Route path={"/auto"} element={<AutoSignaling />} />
         </Routes>
       </BrowserRouter>
     </div>
